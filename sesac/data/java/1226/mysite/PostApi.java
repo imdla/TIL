19f1b08c1bfadd6ea26689a7622bd39af37c3aa0
imdla <imdla918@gmail.com>
2024-12-26 13:37:48 +0900

    [modify] create, read 생성

--- sesac/data/java/1226/mysite/PostApi.java
+++ sesac/data/java/1226/mysite/PostApi.java
@@ -1,4 +1,33 @@
 package com.example.demo.mysite;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
 public class PostApi {
+    List<Post> posts = new ArrayList<Post>();
+    private Long id = 0L; // 게시글 생성에서 id 생성위한 장치
+
+    {
+        // 인스턴스가 생성되었을 때 한 번 실행됨
+        // 초기값을 넣을 때 사용
+        posts.add(new Post(++id, "title", "content"));
+    }
+
+    // create (post / 내용 / url)
+    @GetMapping("/posts/create")
+    public Post createPost() {
+        Post post = new Post(++id, "title", "content");
+        posts.add(post);
+        return post;
+    }
+
+    // read (posts / get)
+    @GetMapping("/posts")
+    public List<Post> readPosts() {
+        return posts;
+    }
 }
