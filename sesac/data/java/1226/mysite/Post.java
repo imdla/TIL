76ebca93c0cc990c08e1e6e280fcb8d2ab67b856
imdla <imdla918@gmail.com>
2024-12-26 11:38:07 +0900

    [add] 간단한 게시판 구현

---
+++ sesac/data/java/1226/mysite/Post.java
@@ -0,0 +1,7 @@
+package com.example.demo.mysite;
+
+public class Post {
+    private Long id;
+    private String title;
+    private String content;
+}
---
+++ sesac/data/java/1226/mysite/PostApi.java
@@ -0,0 +1,4 @@
+package com.example.demo.mysite;
+
+public class PostApi {
+}
