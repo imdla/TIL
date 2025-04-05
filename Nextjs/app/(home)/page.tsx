d9ccb4acc9a53ae348ff22a2f07400e3a3a290ba
imdla <imdla918@gmail.com>
2025-04-05 19:52:33 +0900

    [feat] constants

--- Nextjs/app/(home)/page.tsx
+++ Nextjs/app/(home)/page.tsx
@@ -1,6 +1,6 @@
 import styles from "../../styles/home.module.css";
 import Movie from "../../components/movie";
-import { API_URL } from "../../lib/constants";
+import { API_URL } from "../../constants";
 
 export const metadata = {
   title: "Home",
